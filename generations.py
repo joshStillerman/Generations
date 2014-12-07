d622d05f3053fe04b64f4e8a3a9aaec45aa04c3f
Jstillerman <Jason@Jasons-MBP.hsd1.ma.comcast.net>
2014-12-07 11:39:14 -0500

    Initial Commit of NonRandom Branch
    Instead of picking a random letter, my program picks the next letter.
    This has made it Finnish in insanely less generations than before.

--- generations.py
+++ generations.py
@@ -1,7 +1,8 @@
 import random
 
 
-letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+letters = "* a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+#for whatever reason the first letter of the array is not being recognised anymore
 letters.append(" ")
 
 
@@ -15,7 +16,7 @@ def replace(word, index, letter):
     for thing in tempword:
         retrn = retrn + thing
     return retrn
- 
+
 while True:
     count = 0
     goal = raw_input("Enter Goal text(letters and spaces only): ").lower()
@@ -27,7 +28,7 @@ while True:
     while not done:
         for i in range(0, len(text)):
             if text[i] != goal[i]:
-                 text = replace(text, i, letters[random.randint(0, len(letters)-1)])
+                 text = replace(text, i, letters[letters.index(text[i])%(len(letters)-1)+1])
         print text
         count += 1
         done = True if (text == goal) else False
